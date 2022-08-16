2dd3d40c96df8142e480ffc9a10fe0d086c7de30
SrvS26 <92995533+SrvS26@users.noreply.github.com>
2022-08-16 21:49:32 +0530

    UserID details (#7)
    * add userId to error logs so we know
    
    * UserId added and \n issue sorted
    
    * Removed print statements
    
    * Altered variable names
    
    Co-authored-by: Mrinal Purohit <github@mrinalpurohit.in>

--- app/License.py
+++ app/License.py
@@ -23,9 +23,10 @@ errors = {
 conn = sqlite3.connect(databaseFile)
 
 
+# fetchToken :: () -> [{"user_id"::str, "access_token"::str}]
 def fetchToken():
     cursor = conn.cursor()
-    data = """SELECT access_token FROM USERS WHERE is_validated = 0"""
+    data = """SELECT access_token, user_id FROM USERS WHERE is_validated = 0"""
     try:
         cursor.execute(data)
         records = cursor.fetchall()
@@ -34,7 +35,10 @@ def fetchToken():
     listTokens = []
     if len(records) > 0:
         for item in records:
-            listTokens.append(item[0])
+            user_details = {}
+            user_details["access_token"] = item[0]
+            user_details["user_id"] = item[1]
+            listTokens.append(user_details)
     return listTokens
 
 
@@ -77,7 +81,9 @@ def addLicenseKey(userId, licenseKey):
     return
 
 
-def fetchID(token):
+def fetchID(userDetails):
+    token = userDetails["access_token"]
+    userID = userDetails["user_id"]
     databaseIDurl = "https://api.notion.com/v1/search"
     params = {
         "filter": {"value": "database", "property": "object"},
@@ -100,39 +106,48 @@ def fetchID(token):
             if len(database) > 0:
                 databaseResults = database[0]
                 databaseID = databaseResults.get("id", None)
-                userID = databaseResults.get("created_by", {}).get("id", None)
                 return (databaseID, userID)
             else:
-                logging.error(f"License Key database not found")
+                logging.error(f"License Key database not found for user: ", userID)
                 return (None, None)
         else:
-            logging.error(f"Query for databaseID and userID failed: {statusCode}")
+            logging.error(
+                f"Query for databaseID for user: {userID} failed with {statusCode}"
+            )
             return (None, None)
     except Exception as e:
         logging.exception(f"Query for databaseID and userID failed: {e}")
         return (None, None)
 
-
-def fetchLicenseKey(databaseID, token):
+def fetchLicenseKey(databaseID, userDetails):
+    token = userDetails["access_token"]
+    userID = userDetails["user_id"]
     url = f"https://api.notion.com/v1/databases/{databaseID}/query"
+    payload = {"filter": {
+        "property": "License Key",
+        "rich_text": {"ends_with": ";"}
+    }}
     headers = {
         "Content-Type": "application/json",
         "Notion-Version": "2022-02-22",
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {token}"
     }
     try:
-        response = requests.request("POST", url, headers=headers)
+        response = requests.request("POST", url, json=payload, headers=headers)
         statusCode = response.status_code
         if statusCode == 200:
             parsedResponse = response.json()
-            results = parsedResponse.get("results", [{}])[0]
-            licenseList = (
-                results.get("properties", {}).get("License Key", {}).get("title", [])
-            )
-            if len(licenseList) > 0:
-                licenseKey = licenseList[0].get("plain_text", None)
-                if licenseKey[-1] == ";":
-                    licenseKey = licenseKey[:-1]
+            allResults = parsedResponse.get("results", [{}])
+            if len(allResults) != 0:
+                results = allResults[0]
+                licenseList = (
+                        results.get("properties", {}).get("License Key", {}).get("title", [])
+                    )
+                if len(licenseList) != 0:
+                    recLicenseKey = licenseList[0].get("plain_text", None)
+                        # if licenseKey[-1] == ";":
+                    altLicenseKey = recLicenseKey[:-1]
+                    licenseKey = ''.join(altLicenseKey.splitlines())
                     pageID = results.get("id", None)
                     return licenseKey, pageID
                 else:
@@ -140,14 +155,15 @@ def fetchLicenseKey(databaseID, token):
             else:
                 return None, None
         else:
-            logging.error(f"Could not fetch license key: {statusCode}")
+            logging.error(f"Could not fetch license key for user: {userID}, error: {statusCode}")
             return None, None
     except Exception as e:
-        logging.exception(f"Could not fetch license key:{e}")
+        logging.exception(f"Could not fetch license key for user: {userID}, exception: {e}")
         return None, None
 
 
-def verifyLicenseKey(licenseKey):
+def verifyLicenseKey(licenseKey, userDetails):
+    userID = userDetails["user_id"]
     url = "https://api.gumroad.com/v2/licenses/verify"
     params = {"product_permalink": gumroadProductId, "license_key": licenseKey.strip()}
     try:
@@ -158,15 +174,15 @@ def verifyLicenseKey(licenseKey):
             return (parsed, 100)
         else:
             logging.error(
-                f"Gumroad license key query failed: {statusCode}, license key: {licenseKey}, response: {verify.text}"
+                f"Gumroad license key query failed for user: {userID}, status code: {statusCode}, license key: {licenseKey}, response: {verify.text}"
             )
             return (None, 102)
     except Exception as e:
-        logging.error(f"Gumroad license key query failed: {e}")
+        logging.error(f"Gumroad license key query failed for user: {userID}, {e}")
         return (None, 104)
 
 
-def verifiedResponse(response, userId, licenseKey):
+def verifiedResponse(response, userId, licenseKey, listRevoked):
     if response.get("success") == True:
         numUses = response.get("uses", None)
         if numUses == 1:
@@ -187,7 +203,9 @@ def verifiedResponse(response, userId, licenseKey):
         return 104
 
 
-def error(pageID, value, token, licenseKey):
+def error(pageID, value, userDetails, licenseKey):
+    token = userDetails["access_token"]
+    userID = userDetails["user_id"]
     url = f"https://api.notion.com/v1/pages/{pageID}"
     message = errors[value]
     payload = {
@@ -208,28 +226,35 @@ def error(pageID, value, token, licenseKey):
         )
         statusCode = r.status_code
         if statusCode != 200:
-            logging.error(f"Could not patch message to Notion database:{statusCode}")
+            logging.error(
+                f"Could not patch message to Notion database:{statusCode} for user: {userID}"
+            )
             return
     except Exception as e:
-        logging.exception(f"Could not patch message to Notion database:{e}")
+        logging.exception(
+            f"Could not patch message to Notion database:{e} for user: {userID}"
+        )
         return
 
 
 while True:
     listRevoked = getRevoked()
     listTokens = fetchToken()
-    for token in listTokens:
+    for userDetails in listTokens:
         try:
-            databaseID, userID = fetchID(token)
-            if databaseID is not None and userID is not None:
-                licenseKey, pageID = fetchLicenseKey(databaseID, token)
+            userID = userDetails["user_id"]
+            databaseID, userID = fetchID(userDetails)
+            if databaseID is not None:
+                licenseKey, pageID = fetchLicenseKey(databaseID, userDetails)
                 if licenseKey is not None and pageID is not None:
-                    response = verifyLicenseKey(licenseKey)
+                    response = verifyLicenseKey(licenseKey, userDetails)
                     if response[0] is not None:
-                        value = verifiedResponse(response[0], userID, licenseKey)
-                        error(pageID, value, token, licenseKey)
+                        value = verifiedResponse(
+                            response[0], userID, licenseKey, listRevoked
+                        )
+                        error(pageID, value, userDetails, licenseKey)
                     else:
-                        error(pageID, response[1], token, licenseKey)
+                        error(pageID, response[1], userDetails, licenseKey)
         except Exception as e:
             logging.exception(e)
     time.sleep(5)
